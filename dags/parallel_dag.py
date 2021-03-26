dbaed9552812a6b6456d745fc7ee50a22cf7367e
caio henrique <caiohenriqueads@gmail.com>
2021-03-26 08:37:10 -0300

    - Sugdag example - Xcom example

--- dags/parallel_dag.py
+++ dags/parallel_dag.py
@@ -1,7 +1,10 @@
 from airflow.models import DAG
 from airflow.operators.bash import BashOperator
+from airflow.operators.subdag import SubDagOperator
+from airflow.utils.task_group import TaskGroup
 
 
+from subdags.subdag_parallel_dag import subdag_parallel_dag
 from datetime import datetime
 
 
@@ -16,22 +19,39 @@ with DAG('parallel_dag', schedule_interval='@daily', default_args=default_args, 
         bash_command='sleep 3'
     )
 
+    
+    # processing = SubDagOperator(
+    #     task_id='processing_tasks',
+    #     subdag=subdag_parallel_dag('parallel_dag', 'processing_tasks', default_args)
+    # )
 
-    task_2 = BashOperator(
-        task_id='task_2',
-        bash_command='sleep 3'
-    )
+    with TaskGroup('processing_tasks_group') as processing_tasks_group:
+        task_2 = BashOperator(
+            task_id='task_2',
+            bash_command='sleep 3'
+        )
 
+        with TaskGroup('spark_tasks') as spark_tasks:
+            task_3 = BashOperator(
+                task_id='task_3',
+                bash_command='sleep 3'
+            )
 
-    task_3 = BashOperator(
-        task_id='task_3',
-        bash_command='sleep 3'
-    )
+        with TaskGroup('flink_tasks') as flink_tasks:
+            task_3 = BashOperator(
+                task_id='task_3',
+                bash_command='sleep 3'
+            )   
 
+        task_3 = BashOperator(
+            task_id='task_3',
+            bash_command='sleep 3'
+        )     
+    
 
     task_4 = BashOperator(
         task_id='task_4',
         bash_command='sleep 3'
     )
 
-    task_1 >> [task_2, task_3] >> task_4
+    task_1 >> processing_tasks_group >> task_4
